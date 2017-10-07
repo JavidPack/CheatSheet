b0c60f2950f2b3ad5c36f036e63c019fa3e3fd39
R <rrryutaro@gmail.com>
2017-10-07 19:29:58 +0900

    v0.2.7.4-feature
    - Addition of tooltip information to NPC browser
    - Change Draw in NPCSlot (for drawing slime)

---
+++ ModUtils.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace CheatSheet
+{
+    internal static class ModUtils
+    {
+        internal static Texture2D Resize(this Texture2D texture, int size)
+        {
+            Texture2D result = texture;
+
+            float max =  Math.Max(texture.Width, texture.Height);
+            float scale = size / max;
+            int width = (int)(texture.Width * scale);
+            int height = (int)(texture.Height * scale);
+
+            using (System.IO.MemoryStream ms = new System.IO.MemoryStream())
+            {
+                texture.SaveAsPng(ms, width, height);
+                result = Texture2D.FromStream(texture.GraphicsDevice, ms);
+            }
+            return result;
+        }
+    }
+}
